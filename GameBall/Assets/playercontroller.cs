9fad062de335db0eded899cfdc0fdc4f05b07acf
first0021 <holyduck0021@gmail.com>
2022-02-05 15:03:26 +0700

    first commit

---
+++ GameBall/Assets/playercontroller.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+public float speed;
+    private Rigidbody rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement*speed);
+    }
+   
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("coin")) ;
+       
+    }
+    
+    
+}
